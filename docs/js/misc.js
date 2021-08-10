67f55ee0b7aaf6906359d51e1458a9dc61b77b5c
Debajyoti sarkar <debajyotisarkar.jio1@gmail.com>
2021-08-11 02:17:32 +0530

    fixed button on index

---
+++ docs/js/misc.js
@@ -0,0 +1,4 @@
+invite_url="https://discord.com/oauth2/authorize?client_id=868586634290036766&permissions=171799202880&scope=bot"
+function invite(){
+    window.open(invite_url, '_blank').focus();
+}
